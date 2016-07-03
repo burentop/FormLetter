a6ca91fd5736a1af612e88e0bfa78482e7cfded7
Brent Perry <burentoperii@gmail.com>
2016-07-03 17:45:32 -0500

    Add prompt for input of friend name.

--- FormLetter/main.cpp
+++ FormLetter/main.cpp
@@ -10,10 +10,14 @@
 
 int main()
 {
-    cout << "Please enter the name of the person you want to write to (followed by 'enter'): \n";
+    cout << "Please enter the name of the person you want to write to: ";
     string first_name;
+    string friend_name;
     cin >> first_name;
+    cout << "Please type the name of another friend: ";
+    cin >> friend_name;
     cout << "Dear " << first_name << ",\n";
     cout << "\tHow are you? I am fine. I miss you.\n";
     cout << "I have decided to learn C++.\n";
+    cout << "Have you seen " << friend_name << " lately?\n";
 }
